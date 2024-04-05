124ee78f19e274ee4b18015622a16f5faa86c883
Hugo Vacquier <94376539+Hugo-Vacquier@users.noreply.github.com>
2024-04-05 15:05:23 +0200

    Java doc

--- src/Controller/SimpleTaskManager.java
+++ src/Controller/SimpleTaskManager.java
@@ -6,28 +6,53 @@ import Model.TaskManager;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe implémentant l'interface TaskManager pour fournir une gestion simple des tâches.
+ * Cette implémentation utilise une liste pour stocker les tâches.
+ */
 public class SimpleTaskManager implements TaskManager {
     private List<Task> tasks;
 
+    /**
+     * Constructeur de la classe SimpleTaskManager.
+     * Initialise la liste de tâches.
+     */
     public SimpleTaskManager() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Ajoute une nouvelle tâche à la liste.
+     * @param tache la tâche à ajouter
+     */
     @Override
     public void ajouterTache(Task tache) {
         tasks.add(tache);
     }
 
+    /**
+     * Modifie une tâche existante.
+     * @param tache la tâche à modifier
+     *              (non implémenté dans cette version)
+     */
     @Override
     public void modifierTache(Task tache) {
         // Implémentez la logique de modification de la tâche
     }
 
+    /**
+     * Supprime une tâche de la liste.
+     * @param tache la tâche à supprimer
+     */
     @Override
     public void supprimerTache(Task tache) {
         tasks.remove(tache);
     }
 
+    /**
+     * Récupère la liste de toutes les tâches.
+     * @return la liste de toutes les tâches
+     */
     @Override
     public List<Task> getTaches() {
         return tasks;
--- src/Model/TaskManager.java
+++ src/Model/TaskManager.java
@@ -1,12 +1,34 @@
 package Model;
 
-import Model.Task;
-
 import java.util.List;
 
+/**
+ * Interface définissant un ensemble de méthodes communes pour la gestion des tâches.
+ * Les classes concrètes implémenteront cette interface pour fournir une implémentation spécifique.
+ */
 public interface TaskManager {
+
+    /**
+     * Ajoute une nouvelle tâche.
+     * @param tache la tâche à ajouter
+     */
     void ajouterTache(Task tache);
+
+    /**
+     * Modifie une tâche existante.
+     * @param tache la tâche à modifier
+     */
     void modifierTache(Task tache);
+
+    /**
+     * Supprime une tâche.
+     * @param tache la tâche à supprimer
+     */
     void supprimerTache(Task tache);
+
+    /**
+     * Récupère la liste de toutes les tâches.
+     * @return la liste de toutes les tâches
+     */
     List<Task> getTaches();
 }
